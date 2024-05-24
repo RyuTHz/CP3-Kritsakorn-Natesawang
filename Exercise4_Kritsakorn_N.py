9e303dabc91f2d083aff668679155c9ae0305930
RyuTHz <ryuw.29@gmail.com>
2024-05-24 10:50:36 +0700

    Exercise update

---
+++ Exercise4_Kritsakorn_N.py
@@ -0,0 +1,19 @@
+print("--- Your Score ---")
+print("Type your score Ex.")
+print("100")
+print("50")
+print("70")
+print("....")
+fe = int(input())#Foundation English
+gb = int(input())#General Business
+itcs = int(input())#Introduction to Computer Systems
+cp = int(input())#Computer Programming
+
+print("Foundation English :",fe)#Foundation English
+print("General Business :",gb)#General Business
+print("Introduction to Computer Systems :",itcs)#Introduction to Computer Systems
+print("Computer Programming",cp)#Computer Programming
+
+
+
+
---
+++ Exercise5_2_Kritsakorn_N.py
@@ -0,0 +1,7 @@
+print("This is speed calculator!!")
+print("-----------------------------------------------------")
+distance=float(input("Please type your distance in km: "))
+time=float(input("Please type your time in hour: "))
+speed=distance/time
+print("Here's your answer:",speed,"km/h")
+print("-----------------------------------------------------")
